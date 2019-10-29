49d5700350d4f61e296af9005f3c6e16c525e90b
moe1047 <moe1047@gmail.com>
2019-10-29 20:26:23 +0300

    first commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,36 +1,39 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Account Balances",
+	'name': "Account Balances",
+	'version': '4.5.3',
+	'price': 10.0,
+	'category' : 'Accounting',
+	'license': 'Other proprietary',
+	'currency': 'USD',
+	'summary': """This app allow you to view payment journal balances.""",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+	'description': """
+		- View Payment Journal balance from payment form.
+		- View Payment Journal balance from register payment invoice form.
+	""",
 
-    'description': """
-        Long description of module's purpose
-    """,
+	'author': "Meisour LLc",
+	'website': "http://www.meisour.com",
 
-    'author': "Meisour LLc",
-    'website': "http://www.meisour.com",
+	# Categories can be used to filter modules in modules listing
+	# Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+	# for the full list
+	'category': 'account',
+	'version': '0.1',
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'account',
-    'version': '0.1',
+	# any module necessary for this one to work correctly
+	'depends': ['base', 'account'],
 
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'account'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-        'views/account.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+	# always loaded
+	'data': [
+		# 'security/ir.model.access.csv',
+		'views/views.xml',
+		'views/templates.xml',
+		'views/account.xml',
+	],
+	# only loaded in demonstration mode
+	'demo': [
+		'demo/demo.xml',
+	],
 }
